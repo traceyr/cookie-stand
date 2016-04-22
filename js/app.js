faef44895e5042b0272d137b294fe43369236b97
Tracey <radcliffe.tracey@gmail.com>
2016-04-21 20:16:19 -0700

    fixed a small error in js and finished website

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var hoursOfOpp = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:', 'Totals'];
+var hoursOfOpp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Totals'];
 var appendRows = document.getElementById('storeData');
 var appendHead = document.getElementById('storeHours');
-var newLocationArray = [];
+var locationAry = [];
 
 var storeLocation = function(locationName, minCust, maxCust, aveCookiePerSale){
   this.locationName = locationName;
